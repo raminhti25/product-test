f99efd276e6ba19d40ff48409f1921aab3ba94c8
Ramin Heshmati <r.heshmati@pixaldo.com>
2022-01-20 12:32:10 +0330

    Register VoteObserver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Vote;
 use App\Models\Comment;
 use App\Observers\CommentObserver;
+use App\Observers\VoteObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Vote::observe(VoteObserver::class);
         Comment::observe(CommentObserver::class);
     }
 }
