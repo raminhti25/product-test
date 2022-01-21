b3fac86ab5d05be224dcf287abbb888a69958516
Ramin Heshmati <r.heshmati@pixaldo.com>
2022-01-21 13:38:52 +0330

    Fix condition of commentPolicy

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -12,10 +12,10 @@ class CommentPolicy
 
     public function create(?User $user, $product)
     {
-        if (is_null($user) && !$product->comment_enabled) {
-            return Response::deny(trans('messages.errors.cant_leave_comment'));
+        if (is_null($user) && $product->comment_enabled && $product->edit_by_visitor_enabled) {
+            return Response::allow();
         }
 
-        return Response::allow();
+        return Response::deny(trans('messages.errors.cant_leave_comment'));
     }
 }
